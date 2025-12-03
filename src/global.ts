529dc04f931de44d6122462141467db1ca621b12
Paras Shah <parasshah195@gmail.com>
2025-12-03 11:43:55 +0530

    Update Webflow IX init; remove unused file and gsap register

--- src/global.ts
+++ src/global.ts
@@ -1,22 +1,19 @@
-import './entry';
 import { animatedDetailsAccordions } from '$components/accordions';
 import Dialog from '$components/dialog';
 import NavMenu from '$components/nav/menu';
 import { navbarScrollToggle } from '$components/nav/scroll';
-import { initCursorFollow } from '$utils/cursor-follow';
 import { setCurrentYear } from '$utils/current-year';
+import { initCursorFollow } from '$utils/cursor-follow';
 import '$utils/disable-webflow-scroll';
 import { disableWebflowAnchorSmoothScroll } from '$utils/disable-webflow-scroll';
 import handleExternalLinks from '$utils/external-link';
 import addMainElementId from '$utils/main-element-id';
 import { duplicateMarqueeList } from '$utils/marquee-list';
 
-gsap.registerPlugin(ScrollTrigger);
-
 window.Webflow = window.Webflow || [];
 window.Webflow?.push(() => {
   setTimeout(() => {
-    window.WF_IX = Webflow.require('ix3');
+    window.WF_IX = window.Webflow.require('ix3');
     console.debug('Webflow IX3 globalised:', window.WF_IX);
   }, 100);
 
